100c60a7f5696a675bca8bf5ad815d613278fa5e
agushildan <agushildan358@gmail.com>
2026-07-28 09:52:42 +0700

    keterangan perubahan

--- digi/src/pages/Kegiatan.jsx
+++ digi/src/pages/Kegiatan.jsx
@@ -1,6 +1,9 @@
 function Kegiatan(){
     return(
-        <h1>halo</h1>
+        <div>
+            <h1>halo</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut possimus consequatur adipisci ipsam, soluta odio accusamus veritatis voluptatum illum veniam tempora earum dolorum asperiores a ducimus modi aliquid deserunt consequuntur!</p>
+        </div>
     )
 }
 
